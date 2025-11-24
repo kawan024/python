7a3be1036adec4bec711bb5502de30b7b6983641
kawan024 <kawan.rodrigues.santos@escola.pr.gov.br>
2025-11-24 14:11:41 +0000

    falta acabar

---
+++ Solicitar a entrada de dois numeros.py
@@ -0,0 +1,6 @@
+num1_str = input("Digite o primeiro número: ")
+num2_str = input("Digite o segundo número: ")
+num1_str = float(num1_str)
+num2_str = float(num2_str)
+soma = num1_str + num2_str
+print(f"A soma dos dois números é: {soma}")
